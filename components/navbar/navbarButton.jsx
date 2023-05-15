b134c6ef6b570417a770fce8e127946e2b75b1a9
Helmi Trisna <helmytrisna86@gmail.com>
2023-05-15 12:37:14 +0800

    first commit

--- components/navbar/navbarButton.jsx
+++ components/navbar/navbarButton.jsx
@@ -13,7 +13,6 @@ const NavbarButton = ({ href, name, onClick }) => {
             ? "text-white bg-blue-700 md:text-blue-700 md:p-0 md:dark:text-blue-500"
             : "text-gray-900 hover:bg-gray-100 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
         } `}
-        //
       >
         {name}
       </a>
