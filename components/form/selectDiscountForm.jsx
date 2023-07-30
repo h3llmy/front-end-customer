31aebc511e70050296d0e8c2c20db07567300d89
Helmi Trisna <helmytrisna86@gmail.com>
2023-07-30 14:34:07 +0800

    deploy vercel

--- components/form/selectDiscountForm.jsx
+++ components/form/selectDiscountForm.jsx
@@ -37,7 +37,8 @@ const SelectDiscountForm = ({ children, productId, discountId }) => {
         },
       });
       if (newOrder.data.data.redirect_url) {
-        router.push(newOrder.data.data.redirect_url);
+        // router.push(newOrder.data.data.redirect_url);
+        console.log(newOrder.data.data.redirect_url);
       }
       setTimeout(() => {
         setButtonDisabled(false);
--- utils/fetch.js
+++ utils/fetch.js
@@ -9,6 +9,5 @@ export const fetchApi = axios.create({
   headers: {
     "Content-Type": "application/json",
     "Content-Type": "multipart/form-data",
-    "Access-Control-Allow-Headers": "*",
   },
 });
